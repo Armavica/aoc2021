23dc69fe4646f01db028652cbc89a4c5bba8db7d
Virgile Andreani <armavica@ulminfo.fr>
2021-12-07 00:39:36 -0500

    No negative positions

--- 07/__main__.py
+++ 07/__main__.py
@@ -2,8 +2,8 @@ import fileinput as fi
 
 data = [int(x) for x in next(fi.input()).split(',')]
 
-fuel = min(sum(abs(p-x) for x in data) for p in range(min(data), max(data)+1))
+fuel = min(sum(abs(p-x) for x in data) for p in range(max(data)+1))
 print(fuel)
 
-fuel = min(sum((abs(p-x)*(abs(p-x)+1))//2 for x in data) for p in range(min(data), max(data)+1))
+fuel = min(sum((abs(p-x)*(abs(p-x)+1))//2 for x in data) for p in range(max(data)+1))
 print(fuel)
