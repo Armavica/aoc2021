5d56c69304f20977e66d73b7f20462de045f2f90
Virgile Andreani <armavica@ulminfo.fr>
2021-12-07 00:40:28 -0500

    Useless parentheses

--- 07/__main__.py
+++ 07/__main__.py
@@ -5,5 +5,5 @@ data = [int(x) for x in next(fi.input()).split(',')]
 fuel = min(sum(abs(p-x) for x in data) for p in range(max(data)+1))
 print(fuel)
 
-fuel = min(sum((abs(p-x)*(abs(p-x)+1))//2 for x in data) for p in range(max(data)+1))
+fuel = min(sum(abs(p-x)*(abs(p-x)+1)//2 for x in data) for p in range(max(data)+1))
 print(fuel)
