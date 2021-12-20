a77d64ff1e154080d6e4520b27c48852a99ffe8f
Virgile Andreani <armavica@ulminfo.fr>
2021-12-20 00:47:04 -0500

    Simplify day 20

--- 20/__main__.py
+++ 20/__main__.py
@@ -6,20 +6,12 @@ inpt = fi.input()
 
 rules = np.array([c == '#' for c in next(inpt).rstrip()], dtype=np.int_)
 next(inpt)
+grid = np.array([[c == '#' for c in line.rstrip()] for line in inpt], dtype=np.bool_)
 
-grid = []
-for line in inpt:
-    line = line.rstrip()
-    grid.append([[0, 1][c == '#'] for c in line])
-grid = np.array(grid, dtype=np.bool_)
-
-def step(grid, fillvalue):
-    con = convolve2d(grid, np.array(2**np.arange(9).reshape((3, 3))), fillvalue=fillvalue)
-    return rules[con]
-
+kernel = np.array(2**np.arange(9).reshape((3, 3)))
 fillvalue = 0
 for i in range(50):
-    grid = step(grid, fillvalue)
+    grid = rules[convolve2d(grid, kernel, fillvalue=fillvalue)]
     fillvalue = rules[511 * fillvalue]
     if i == 1:
         print(np.sum(grid))
